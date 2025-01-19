50885c00397ec6477e3c6cf8cdfa7797f9a55329
nekitoksik <nikita2018top@gmail.com>
2025-01-19 19:29:36 +0300

    add post and delete for users

--- app/users/router.py
+++ app/users/router.py
@@ -3,6 +3,9 @@ from fastapi import APIRouter, Response
 from app.users.schemas import SUser
 from app.users.services import UserService
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import async_session_maker
+
 router = APIRouter(
     prefix="/users",
     tags=["Пользователи"],
@@ -13,3 +16,13 @@ async def get_users() -> list[SUser]:
     result = await UserService.get_all()
 
     return result
+
+@router.post("", response_model=SUser, status_code=201)
+async def create_user(user: SUser):
+    return await UserService.create_user(user)
+
+
+@router.delete("/{user_id}", status_code=204)
+async def delete_user(user_id: int):
+    await UserService.delete_user(user_id)
+    return {}
--- app/users/services.py
+++ app/users/services.py
@@ -1,21 +1,35 @@
+from fastapi import HTTPException
 from app.users.models import Users
 from app.services.base import BaseService
 
+from app.users.schemas import SUser
+
 from app.database import async_session_maker
-from sqlalchemy import select
+from sqlalchemy import select, delete
 
 class UserService(BaseService):
     model = Users
 
-    # @classmethod
-    # async def get_user_by_id(cls, user_id: int) -> SUserSecure:
-    #     async with async_session_maker() as session:
-    #         query = select(cls.model.id, cls.model.email, cls.model.name).filter_by(id=user_id)
-    #         result = await session.execute(query)
-            
-    #         user_data = result.fetchone()
-    #         if user_data:
-    #             return SUserSecure(id=user_data.id, email=user_data.email, name=user_data.name)  
-    #         return None
+    @classmethod
+    async def create_user(cls, user_data: SUser) -> SUser:
+        """Создает нового пользователя."""
+        async with async_session_maker() as session:
+            db_user = Users(**user_data.model_dump(exclude_unset=True))
+            session.add(db_user)
+            await session.commit()
+            await session.refresh(db_user)
+            return SUser.model_validate(db_user)
 
-    
+    @classmethod
+    async def delete_user(cls, user_id: int) -> None:
+        async with async_session_maker() as session:
+            query = delete(cls.model).where(cls.model.id == user_id)
+            await session.execute(query)
+            await session.commit()
+
+            try:
+                get_user = select(cls.model.name).filter_by(id=user_id)
+                get_user_result = await session.execute(get_user)
+                raise HTTPException(status_code=500, detail="Пользователь не удален")
+            except:
+                pass
