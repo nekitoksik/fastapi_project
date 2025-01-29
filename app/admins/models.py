05419ee9a12440c3fb762577da40bd52cda1f300
nekitoksik <144992337+nekitoksik@users.noreply.github.com>
2025-01-29 22:25:13 +0300

    Update models.py

--- app/admins/models.py
+++ app/admins/models.py
@@ -4,5 +4,5 @@ from sqlalchemy import Column, String, Integer
 class Admins(Base):
     __tablename__ = "admins"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
