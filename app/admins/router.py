a565bd1b87799afb362517c32fc3084fe16b77d7
Flexerr22 <147339081+Flexerr22@users.noreply.github.com>
2025-01-30 00:15:50 +0300

    Update router.py

--- app/admins/router.py
+++ app/admins/router.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, Response
+from fastapi.responses import JSONResponse
 from app.exceptions import IncorrectUserEmailOrPasswordException, UserAlreadyExcist
-
-from app.admins.models import Admins
 from app.admins.services import AdminService
 from app.admins.schemas import SAdminAuth
 from app.admins.auth import get_password_hash, verify_password, create_access_token, verify_user
-
 
 router = APIRouter(
     prefix='/admin',
@@ -14,28 +12,47 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_admin(admin_data: SAdminAuth):
-
     existing_admin = await AdminService.find_one_or_none(email=admin_data.email)
-
     if existing_admin:
         raise UserAlreadyExcist
     
     hashed_password = get_password_hash(admin_data.password)
     await AdminService.add(email=admin_data.email, password=hashed_password)
 
+    response = JSONResponse(content={"message": "Admin registered"})
+    response.headers["Access-Control-Allow-Origin"] = "http://aikanadmin.ru"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    return response
+
 
 @router.post("/login")
 async def login_user(response: Response, admin_data: SAdminAuth):
-    
     admin = await verify_user(admin_data.email, admin_data.password)
     if not admin:
         raise IncorrectUserEmailOrPasswordException
+    
     access_token = create_access_token({"sub": str(admin.id)})    
-    response.set_cookie("tatrun_token", access_token, httponly=True)
-    return access_token
+    response.set_cookie(
+        "tatrun_token", 
+        access_token, 
+        httponly=True, 
+        samesite="None",  # Разрешает кросс-доменные куки
+        secure=True
+    )
 
+    json_response = JSONResponse(content={"access_token": access_token})
+    json_response.headers["Access-Control-Allow-Origin"] = "http://aikanadmin.ru"
+    json_response.headers["Access-Control-Allow-Credentials"] = "true"
+    
+    return json_response
 
 
 @router.post("/logout")
 async def logout_user(response: Response):
     response.delete_cookie("tatrun_token")
+    
+    json_response = JSONResponse(content={"message": "Logged out"})
+    json_response.headers["Access-Control-Allow-Origin"] = "http://aikanadmin.ru"
+    json_response.headers["Access-Control-Allow-Credentials"] = "true"
+
+    return json_response
